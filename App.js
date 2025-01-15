f3a83cd1af45c4fd1528c4bb8d3b92873dcafd5f
NETCHANOK61 <netchanok.p415@gmail>
2025-01-15 13:50:10 +0700

    apk!

--- App.js
+++ App.js
@@ -1,44 +1,3 @@
-// // App.js
-// import React, { useState, useEffect } from "react";
-// import { StatusBar } from "expo-status-bar";
-// import { NavigationContainer } from "@react-navigation/native";
-// import AppNavigator from "./src/app/navigations/AppNavigator";
-// import { View } from "react-native";
-// import { getProfile } from "./src/app/utils/Storage";
-
-// // Redux
-// import { Provider } from "react-redux";
-// import store from "./src/app/store/store"; // Import the store
-
-// export default function App() {
-//   const [showLogin, setShowLogin] = useState(false);
-
-//   const init = () => {
-//     getProfile().then((profile) => {
-//       setShowLogin(profile == null ? true : false);
-//     });
-//   };
-
-//   useEffect(() => {
-//     init();
-//   });
-
-//   return (
-//     <Provider store={store}>
-//     <NavigationContainer>
-//       <StatusBar
-//         barStyle="dark-content"
-//         backgroundColor="#ffffff"
-//         hidden={false}
-//       />
-//       <View style={{ flex: 1 }}>
-//         <AppNavigator showAuthen={showLogin} />
-//       </View>
-//     </NavigationContainer>
-//     </Provider>
-//   );
-// }
-
 import React, { useState, useEffect } from "react";
 import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
@@ -47,7 +6,7 @@ import { View, Text } from "react-native";
 import store from "./src/app/store/store";
 import AppNavigator from "./src/app/navigations/AppNavigator";
 import { getProfile } from "./src/app/utils/Storage";
-import { useFonts } from "expo-font";
+// import { useFonts } from "expo-font";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -63,13 +22,13 @@ const App = () => {
   }, []);
 
   // โหลดฟอนต์
-  const [fontsLoaded] = useFonts({
-    "Prompt-Bold": require("./assets/fonts/Prompt-Bold.ttf"),
-    "Prompt-Light": require("./assets/fonts/Prompt-Light.ttf"),
-    "Prompt-Medium": require("./assets/fonts/Prompt-Medium.ttf"),
-    "Prompt-Regular": require("./assets/fonts/Prompt-Regular.ttf"),
-    "Prompt-Thin": require("./assets/fonts/Prompt-Thin.ttf"),
-  });
+  // const [fontsLoaded] = useFonts({
+  //   "Prompt-Bold": require("./assets/fonts/Prompt-Bold.ttf"),
+  //   "Prompt-Light": require("./assets/fonts/Prompt-Light.ttf"),
+  //   "Prompt-Medium": require("./assets/fonts/Prompt-Medium.ttf"),
+  //   "Prompt-Regular": require("./assets/fonts/Prompt-Regular.ttf"),
+  //   "Prompt-Thin": require("./assets/fonts/Prompt-Thin.ttf"),
+  // });
 
   return (
     <Provider store={store}>
@@ -80,8 +39,9 @@ const App = () => {
           hidden={false}
         />
         <View style={{ flex: 1 }}>
-          {!fontsLoaded && <Text>Loading...</Text>}
+          {/* {!fontsLoaded && <Text>Loading...</Text>} */}
           <AppNavigator showAuthen={showLogin} />
+          {/* <AppNavigator /> */}
         </View>
       </NavigationContainer>
     </Provider>
